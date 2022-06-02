2b4f210aeec75cf7ba88243126b16d5116540a94
Timmy O'Mahony <hey@timmyomahony.com>
2022-06-02 14:10:57 +0100

    Rebuilt modal to mirror asset overlay

--- src/assets/MatrixFieldPreview/dist/js/BlockTypeModal.js
+++ src/assets/MatrixFieldPreview/dist/js/BlockTypeModal.js
@@ -2,10 +2,15 @@ var MFP = MFP || {};
 
 (function ($) {
   /**
-   * Block Type Modal
+   * Matrix Field Preview block type modal
+   * 
+   * A modal dialog based on the default Garnish modal code. This is also 
+   * built and styled with similar functionality to the default asset overlay
+   * used on elements with a sidebar and search toolbar.
    *
-   * A custom modal that extends the default Craft modal to help house our
-   * custom block types
+   * Useful links:
+   * 
+   * - https://github.com/craftcms/cms/blob/2d53a30c99b356ba79705f2a9181706e7c39b388/src/web/assets/garnish/src/Modal.js
    */
   MFP.BlockTypeModal = Garnish.Modal.extend({
     $container: undefined,
@@ -19,95 +24,227 @@ var MFP = MFP || {};
     $grid: undefined,
 
     init: function (container, settings, config, defaultImageUrl) {
-      Garnish.Modal.prototype.init.call(this, container, settings);
-      
       this.config = config;
       this.defaultImageUrl = defaultImageUrl;
       this.searching = false;
 
-      this.buildModal.call(this);
-      this.buildGridItems.call(this);
-      this.buildSearchBar.call(this);
+      settings['resizable'] = true;
+
+      Garnish.Modal.prototype.init.call(this, container, settings);
+
+      this.buildModalHtml.call(this);
+      
+      
+
+      
+      // this.buildSidebarHtml.call(this);
+      // this.buildGridItems.call(this);
+      // this.buildSearchBar.call(this);
 
       // HACK: This seems like the only way to resize.
       this.desiredHeight = 1000;
       Garnish.Modal.prototype.updateSizeAndPosition.call(this);
 
-      this.on(
-        "show",
-        function () {
-          // HACK: For some reason, this is the only way to get autofocus on
-          // open working. It might have something to do with the way the modal
-          // fades in - possibly the input isn't visible/ready at this time.
-          setTimeout(
-            function () {
-              this.$searchInput.focus();
-            }.bind(this),
-            500
-          );
-        }.bind(this)
-      );
+      // this.on(
+      //   "show",
+      //   function () {
+      //     // HACK: For some reason, this is the only way to get autofocus on
+      //     // open working. It might have something to do with the way the modal
+      //     // fades in - possibly the input isn't visible/ready at this time.
+      //     setTimeout(
+      //       function () {
+      //         this.$searchInput.focus();
+      //       }.bind(this),
+      //       500
+      //     );
+      //   }.bind(this)
+      // );
 
-      this.on(
-        "hide",
-        function () {
-          this.resetSearch();
-        }.bind(this)
-      );
+      // this.on(
+      //   "hide",
+      //   function () {
+      //     this.resetSearch();
+      //   }.bind(this)
+      // );
     },
 
-    buildModal: function () {
-      this.$container.addClass("modal mfp-modal");
+    buildSidebarHtml: function() {
+      var sidebar = $('<aside class="mfp-modal__sidebar sidebar"/>');
 
-      this.$body = $('<div class="mfp-modal__body body"/>');
-      this.$footer = $('<footer class="mfp-modal__footer footer"/>');
-      this.$toolbar = $(
-        '<div class="mfp-modal__footer__toolbar toolbar flex flex-nowrap">'
-      );
-      this.$buttons = $(
-        '<div class="mfp-modal__footer__toolbar__buttons buttons right"/>'
-      );
-      this.$cancelBtn = $(
-        '<div class="btn">' + Craft.t("app", "Close") + "</div>"
-      );
+      var sidebarNav = $('<nav class="mfp-modal__sidebar__nav" />');
+      var sidebarUl = $('<ul class="mfp-modal__sidebar__ul" />');
 
-      this.$body.appendTo(this.$container);
-      this.$footer.appendTo(this.$container);
-      this.$toolbar.appendTo(this.$footer);
-      this.$buttons.appendTo(this.$toolbar);
-      this.$cancelBtn.appendTo(this.$buttons);
+      // Add link for each category
+      $.each(this.config["categories"], function(i, category) {
+        var sidebarLi = $("<li class='mfp-modal__sidebar__li'></li>").append(
+          $("<a class='mfp-modal__sidebar__a' />").text(category.name).attr("data-category", category.id)
+        );
+        sidebarUl.prepend(sidebarLi);
+      });
+      sidebar.append(sidebarNav.append(sidebarUl));
 
-      this.$cancelBtn.on(
-        "click",
-        function () {
-          this.hide();
+      // Add default 'All' option
+      var sidebarDefaultLi = $("<li class='mfp-modal__sidebar__li'></li>").append(
+        $("<a class='mfp-modal__sidebar__a sel' />")
+          .text(Craft.t("matrix-field-preview", "All Categories"))
+          .attr("data-category", undefined)
+      );
+      sidebarUl.prepend(sidebarDefaultLi);
+
+      return sidebar;
+    },
+
+    buildFooterHtml: function() {
+      var footer = $('<footer class="mfp-modal__footer footer"/>');
+      // var footerToolbar = $('<div class="mfp-modal__footer__toolbar toolbar flex flex-nowrap">');
+      var footerButtons = $('<div class="mfp-modal__footer__toolbar__buttons buttons right"/>').append(
+          $('<div class="btn">' + Craft.t("matrix-field-preview", "Close") + "</div>")
+      );
+      // var footerSearch = $('<div class="mfp-modal__footer__toolbar__buttons buttons left"/>').append(
+      //   $(
+      //     '<input class="text" type="text" placeholder="Search" dir="ltr" aria-label="Search" />'
+      //   )
+      // ); 
+      
+      footer.append(footerButtons); //.append(footerSearch);
+
+      return footer;
+    },
+
+    buildToolbarHtml: function () {
+      var toolbar = $("<div class='mfp-modal__toolbar toolbar flex flex-nowrap' />").append(
+        $("<div class='mfp-modal__toolbar__search flex-grow texticon search icon clearable' />").append(
+          $('<input class="mfp-modal__toolbar__search__input text fullwidth" type="text" placeholder="Search" dir="ltr" aria-label="Search" />')
+        )
+      );
+      return toolbar;
+    },
+
+    buildGridItemsHtml: function () {
+      var gridContainer = $("<div />", {
+        class: "mfp-modal__grid"
+      })
+      var grid = $("<ul />", { class: "mfp-grid"});
+
+      $.each(
+        this.config["blockTypes"],
+        function (i, blockTypeConfig) {
+          var gridItem = $("<li>", {
+            class: "mfp-grid-item",
+          })
+            .attr("data-block-type", blockTypeConfig.handle.toLowerCase())
+            .attr("data-name", blockTypeConfig.name.toLowerCase())
+            .attr(
+              "data-description",
+              blockTypeConfig.description.toLowerCase()
+            );
+
+          var imgContainer = $("<button>", {
+            class: "mfp-grid-item__button mfp-grid-item__button--default",
+          })
+            .attr("tabindex", 0)
+            .attr("role", "button");
+
+          var img = $("<img>").attr("src", this.defaultImageUrl);
+
+          var content = $("<div>", {
+            class: "mfp-grid-item__content",
+          });
+
+          var name = $("<h2>", {
+            class: "mfp-grid-item__content__name",
+            text: blockTypeConfig.name,
+          });
+
+          var description = $("<div>", {
+            class: "mfp-grid-item__content__description",
+          });
+          if (blockTypeConfig["image"]) {
+            imgContainer.removeClass("mfp-grid-item__button--default");
+            img.attr("src", blockTypeConfig["image"]);
+          }
+          if (blockTypeConfig["name"]) {
+            name.text(blockTypeConfig["name"]);
+          }
+          if (blockTypeConfig["description"]) {
+            description.html(blockTypeConfig["descriptionHTML"]);
+          }
+
+          img.appendTo(imgContainer);
+          name.appendTo(content);
+          description.appendTo(content);
+          gridItem.prepend(imgContainer, content);
+
+          // var onClickHandler = function () {
+          //   this.trigger("gridItemClicked", {
+          //     config: blockTypeConfig,
+          //   });
+          // };
+
+          // When an item is clicked, insert it
+          // imgContainer.on("click", onClickHandler.bind(this));
+          // name.on("click", onClickHandler.bind(this));
+
+          grid.append(gridItem);
         }.bind(this)
       );
+      
+      gridContainer.append(grid);
+      return gridContainer;
     },
 
-    buildSearchBar: function () {
-      // Create elements
-      this.$searchInputContainer = $(
-        '<div class="mfp-modal__footer__toolbar__search flex-grow texticon search icon clearable" />'
-      );
-      this.$searchInput = $(
-        '<input class="text" type="text" placeholder="Search" dir="ltr" aria-label="Search" />'
-      );
-      this.$searchEmpty = $(
-        '<div class="mfp-modal__empty"><span>No results</span></div>'
-      );
+    /**
+     * Build the modal interface
+     * 
+     * This is based on the asset overlay modal.
+     */
+    buildModalHtml: function () {
+      this.$container.addClass("mfp-modal modal elementselectormodal");
 
-      this.$searchInput.appendTo(this.$searchInputContainer);
-      this.$searchInputContainer.prependTo(this.$toolbar);
-      this.$searchEmpty.appendTo(this.$body);
+      var body = $('<div class="mfp-modal__body body has-sidebar"/>');
+      var content = $('<div class="mfp-modal__content content has-sidebar"/>');
+      var main = $('<main class="mfp-modal__main main"/>');
+      var toolbar = this.buildToolbarHtml();
+      var grid = this.buildGridItemsHtml();
+      var sidebar = this.buildSidebarHtml();
+      var footer = this.buildFooterHtml();
+      
+      body.append(content);
+      main.append(toolbar).append(grid);
+      content.append(sidebar).append(main)
+      this.$container.append(body).append(footer);
 
-      this.$searchInput.on(
-        "keyup",
-        this.debounce(function (ev) {
-          this.search(ev.target.value);
-        }.bind(this), 400)
-      );
+      // footerCancel.on(
+      //   "click",
+      //   function () {
+      //     this.hide();
+      //   }.bind(this)
+      // );
     },
+
+    // buildSearchBar: function () {
+    //   // Create elements
+    //   this.$searchInputContainer = $(
+    //     '<div class="mfp-modal__footer__toolbar__search flex-grow texticon search icon clearable" />'
+    //   );
+    //   this.$searchInput = $(
+    //     '<input class="text" type="text" placeholder="Search" dir="ltr" aria-label="Search" />'
+    //   );
+    //   this.$searchEmpty = $(
+    //     '<div class="mfp-modal__empty"><span>No results</span></div>'
+    //   );
+
+    //   this.$searchInput.appendTo(this.$searchInputContainer);
+    //   this.$searchInputContainer.prependTo(this.$toolbar);
+    //   this.$searchEmpty.appendTo(this.$body);
+
+    //   this.$searchInput.on(
+    //     "keyup",
+    //     this.debounce(function (ev) {
+    //       this.search(ev.target.value);
+    //     }.bind(this), 400)
+    //   );
+    // },
 
     buildGridItems: function () {
       this.$grid = $("<ul>", {
@@ -115,7 +252,7 @@ var MFP = MFP || {};
       });
 
       $.each(
-        this.config,
+        this.config["blockTypes"],
         function (i, blockTypeConfig) {
           var $gridItem = $("<li>", {
             class: "mfp-grid-item",
