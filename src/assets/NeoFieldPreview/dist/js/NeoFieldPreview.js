a813d52c6b4c331ac46a7631815be0620f6ea9e9
Timmy O'Mahony <hey@timmyomahony.com>
2023-10-04 14:10:31 +0100

    Fine tuned neo field UI to fix some bugs with button display

--- src/assets/NeoFieldPreview/dist/js/NeoFieldPreview.js
+++ src/assets/NeoFieldPreview/dist/js/NeoFieldPreview.js
@@ -116,6 +116,7 @@ var MFP = MFP || {};
         neoChildBlockTypes.length == 1 &&
         config["field"]["enableTakeover"]
       ) {
+        neoBlock.$container.addClass("mfp-nested-field--disable-single-child");
         return;
       }
       
@@ -184,7 +185,7 @@ var MFP = MFP || {};
       neoBlock.on(
         "addBlockAbove",
         function (ev) {
-          neoInput._tempButtons.$container.css("height", "auto");
+          neoInput._tempButtons.$container.css("height", "35px");
 
           var inlineModalButton = this.createModalButton(
             neoInput._tempButtons.$container,
