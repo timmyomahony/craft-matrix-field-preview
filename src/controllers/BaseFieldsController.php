7fcf3357ba7bdbe338a2b9e3f9c0b90efab60d8e
Timmy O'Mahony <hey@timmyomahony.com>
2025-05-08 13:26:49 +0100

    added form field for new button label on field configs

--- src/controllers/BaseFieldsController.php
+++ src/controllers/BaseFieldsController.php
@@ -108,6 +108,7 @@ abstract class BaseFieldsController extends Controller
 
         $fieldConfig->enablePreviews = $this->request->getBodyParam('enablePreviews');
         $fieldConfig->enableTakeover = $this->request->getBodyParam('enableTakeover');
+        $fieldConfig->buttonLabel = $this->request->getBodyParam('buttonLabel');
 
         if (! $service->save($fieldConfig)) {
             $this->setFailFlash(Craft::t('matrix-field-preview', 'Couldn\'t save the field config.'));
